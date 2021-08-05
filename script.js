a1bda036c7fd945a1c16c318027521d0be0215a0
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-08-04 23:54:53 -0300

    Luan Ferreira Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let boxTop = 200;
+let boxLeft = 200;
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    console.log('keydown event\n\n' + 'key: ' + keyName);
+});
+        
