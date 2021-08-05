d0dccc146fbd8d674f9ba4afd818f62ada14c294
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-08-05 15:34:54 -0300

    Adicionando movimentação e imagem

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 'use strict';
-
+const box = document.getElementById("box")
 let boxTop = 200;
 let boxLeft = 200;
 
-document.addEventListener('keydown', (event) => {
+function keyListener(event){
     const keyName = event.key;
     console.log('keydown event\n\n' + 'key: ' + keyName);
-});
+    console.log(typeof keyName);
+
+//Movimentação vertical
+    if(keyName === 'ArrowUp'){
+        box.style.top = (boxTop - 10) + "px";
         
+    }
+    else if(keyName === 'ArrowDown'){
+        box.style.top = (boxTop + 10) + "px";
+    }
+//Movimentação lateral
+    if(keyName === 'ArrowLeft'){
+        box.style.left = (boxLeft - 10) + "px";
+    }
+    else if(keyName === 'ArrowRight'){
+        box.style.left = (boxLeft + 10) + "px";
+    }
+}
+
+document.addEventListener('keydown', keyListener);
