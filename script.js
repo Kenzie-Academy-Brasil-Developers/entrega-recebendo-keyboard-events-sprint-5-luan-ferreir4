979739744f7bdb7b15906ce0206212541f486da3
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-08-05 22:39:49 -0300

    Movimentação aprimoranda

--- script.js
+++ script.js
@@ -6,22 +6,25 @@ let boxLeft = 200;
 function keyListener(event){
     const keyName = event.key;
     console.log('keydown event\n\n' + 'key: ' + keyName);
-    console.log(typeof keyName);
+    // console.log(typeof keyName);
 
 //Movimentação vertical
     if(keyName === 'ArrowUp'){
-        box.style.top = (boxTop - 10) + "px";
-        
+        box.style.top = (boxTop - 10) + 'px';
+        boxTop -= 10;
     }
     else if(keyName === 'ArrowDown'){
-        box.style.top = (boxTop + 10) + "px";
+        box.style.top = (boxTop + 10) + 'px';
+        boxTop += 10;
     }
-//Movimentação lateral
+//Movimentação horizontal
     if(keyName === 'ArrowLeft'){
-        box.style.left = (boxLeft - 10) + "px";
+        box.style.left = (boxLeft - 10) + 'px';
+        boxLeft -= 10;
     }
     else if(keyName === 'ArrowRight'){
-        box.style.left = (boxLeft + 10) + "px";
+        box.style.left = (boxLeft + 10) + 'px';
+        boxLeft += 10;
     }
 }
 
